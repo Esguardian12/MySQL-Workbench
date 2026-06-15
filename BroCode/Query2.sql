87b596ae68d26f280934e692cf8206b7b734ee6f
Esgardo <45113081+Esguardian12@users.noreply.github.com>
2026-06-15 16:51:59 -0400

    Create Query2.sql

---
+++ BroCode/Query2.sql
@@ -0,0 +1,30 @@
+SELECT last_name, first_name
+FROM employees;
+
+SELECT *
+FROM employees
+WHERE employee_id = 1;
+
+SELECT *
+FROM employees
+WHERE hourly_pay >= 15;
+
+SELECT *
+FROM employees
+WHERE hire_date <= "2023-01-03"
+
+SELECT *
+FROM employees
+WHERE employee_id != 1;
+
+SELECT *
+FROM employees
+WHERE employee_id = 1;
+
+SELECT *
+FROM employees
+WHERE hire_date IS NULL;
+
+SELECT *
+FROM employees
+WHERE hire_date IS NOT NULL;
